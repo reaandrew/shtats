7184f77f0c08a1fb5b6dcbcb1f2c9f50807ba016
Andy Rea <email@andrewrea.co.uk>
2022-01-24 21:59:41 +0000

    Using lines() for number of commits

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,92 @@
 use std::io::{BufRead, BufReader};
 use std::process::{Command, Stdio};
 
-fn chomp_line(line: String){
-	match line.trim_start().chars().collect::<Vec<char>>().as_slice(){
-		['c','o','m','m','i','t',..] => {
-			println!("We have a commit");
-		},
-		['A','u','t','h','o','r',..] => {
-			println!("We have an Author")
-		},
-		['D','a','t','e',..] => {
-			println!("We have a Date")
-		},
-		['S','u','b','j','e','c','t',..] => {
-			println!("We have a Date")
-		},
-		[':',..] => {
-			println!("We have a file stat")
-		},
-		[a,..] if a.is_numeric() => {
-			println!("We have a line count file stat")
-		}
-		&[] => {},
-		&[_, ..] => {}
-	}
+#[derive(Default, Clone, PartialEq)]
+struct GitCommit {
+    commit_hash: String,
+    tags: Vec<String>,
+    author: GitAuthor
+}
+
+fn chomp_commit(line: String, commit: &GitCommit) -> GitCommit {
+    let commit_hash = &line[13..52];
+    let remainder = &line[52..]
+        .replace(&['(', ')'][..], "");
+    let tags = remainder
+        .split(",")
+        .filter(|x| x.contains("tag"))
+        .map(|x| String::from(&x[5..]))
+        .collect::<Vec<String>>();
+
+    let mut commit_clone = commit.clone();
+    commit_clone.commit_hash = commit_hash.into();
+    commit_clone.tags = tags;
+
+    return commit_clone;
+}
+
+fn process(_: &GitCommit, stats: &mut Stats){
+    stats.commit_count += 1;
+
+    if stats.commit_count %100 == 0 {
+        println!("processed {}", stats.commit_count);
+    }
+}
+
+#[derive(Default)]
+struct Stats{
+    commit_count: i32
 }
 
 fn main() {
-    
     let args = vec![
         "--no-pager",
-		"log",
-		"--pretty=format:commit %w(0,5,5) %H%d%nAuthor: %an <%ae> %nDate: %ad%nSubject: %s%nBody: %b%n",
-		"--tags",
-		"--all",
-		"--raw",
-		"--date-order",
-		"--reverse",
-		"--numstat",
-		"--date=rfc2822",
+        "log",
+        "--pretty=format:commit %w(0,5,5) %H%d%nAuthor: %an <%ae> %nDate: %ad%nSubject: %s%n%b",
+        "--tags",
+        "--all",
+        "--raw",
+        "--date-order",
+        "--reverse",
+        "--numstat",
+        "--date=rfc2822",
     ];
+
+    let mut stats: Stats = Default::default();
 
     let stdout = Command::new("git")
         .args(&args)
         .stdout(Stdio::piped())
         .spawn()
-		.expect("Could not spawn git process.")
+        .expect("Could not spawn git process.")
         .stdout
         .expect("Could not capture standard output.");
 
     let reader = BufReader::new(stdout);
 
+    let mut current: GitCommit = Default::default();
+
     reader
         .lines()
-        .filter_map(|line| line.ok())
+        .filter_map(|x|x.ok())
         .for_each(|line| {
-			println!("{}", line);
-			chomp_line(line);
-		});
+            println!("{}", line);
+            match line.chars().collect::<Vec<char>>().as_slice() {
+                ['c', 'o', 'm', 'm', 'i', 't', ..] if current.commit_hash != "" => {
+                    if current != Default::default(){
+                        process(&current, &mut stats);
+                    }
+                    current = Default::default();
+                    current = chomp_commit(line, &current);
+                }
+                &[] => {}
+                &[_, ..] => {}
+            }
+        });
+
+    process(&current, &mut stats);
+
+    println!("Commit Count: {}", stats.commit_count);
 }
+
+mod tests {}
