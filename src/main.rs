7df5dc78721d3d82d4db8b4e9f5c4d0279149e3a
Andy Rea <email@andrewrea.co.uk>
2022-01-25 00:23:12 +0000

    Changed again to use match without pretty

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ use std::process::{Command, Stdio};
 #[derive(Default, Clone, PartialEq)]
 struct GitCommit {
     commit_hash: String,
-    tags: Vec<String>
+    tags: Vec<String>,
+    Author: String,
+    Date: String,
+    Message: Vec<String>,
+    Files: Vec<String>,
+    Lines: Vec<String>,
 }
 
-fn chomp_commit(line: &String, commit: &GitCommit) -> GitCommit {
+fn chomp_commit(line: &String, commit: &mut GitCommit) {
     let commit_hash = &line[..39];
     let remainder = &line[39..]
         .replace(&['(', ')'][..], "");
@@ -17,36 +22,30 @@ fn chomp_commit(line: &String, commit: &GitCommit) -> GitCommit {
         .map(|x| String::from(&x[5..]))
         .collect::<Vec<String>>();
 
-    let mut commit_clone = commit.clone();
-    commit_clone.commit_hash = commit_hash.into();
-    commit_clone.tags = tags;
-
-    return commit_clone;
+    commit.commit_hash = commit_hash.into();
+    commit.tags = tags;
 }
 
-fn process(_: &GitCommit, stats: &mut Stats){
+fn process(commit: &GitCommit, stats: &mut Stats) {
     stats.commit_count += 1;
 
-    if stats.commit_count %1000 == 0 {
+    if stats.commit_count % 1000 == 0 {
         println!("processed {}", stats.commit_count);
     }
 }
 
 #[derive(Default)]
-struct Stats{
-    commit_count: i32
+struct Stats {
+    commit_count: i32,
 }
 
 fn main() {
     let args = vec![
         "--no-pager",
         "log",
-        "-z",
-        "--pretty=format:%ncommit %w(0,0,1) %H%d%nAuthor: %an <%ae> %nDate: %ad%nSubject: %s%b",
-        "--tags",
         "--all",
         "--raw",
-        "--date-order",
+        "--decorate",
         "--reverse",
         "--numstat",
         "--date=rfc2822",
@@ -63,52 +62,40 @@ fn main() {
         .expect("Could not capture standard output.");
 
     let mut reader = BufReader::new(stdout);
-
     let mut current: GitCommit = Default::default();
-
     let mut s = String::new();
 
-    loop{
+    loop {
         s.clear();
         let res = reader.read_line(&mut s);
         if res.is_err() || res.unwrap() == 0 {
             break;
         }
-        println!("{}", s);
         match s.chars().collect::<Vec<char>>().as_slice() {
             ['c', 'o', 'm', 'm', 'i', 't', ..] => {
-                if current != Default::default(){
-                    process(&current, &mut stats);
-                }
+                process(&current, &mut stats);
                 current = Default::default();
-                current = chomp_commit(&s, &current);
+                chomp_commit(&s, &mut current);
+            }
+            ['A', 'u', 't', 'h', 'o', 'r', ..] => {
+                current.Author = s.clone();
+            }
+            ['D', 'a', 't', 'e', ..] => {
+                current.Date = s.clone();
+            }
+            [':', ..] => {
+                current.Files.push(s.clone())
+            }
+            [a, ..] if a.is_numeric() => {
+                current.Lines.push(s.clone())
             }
             &[] => {}
-            &[_, ..] => {}
+            &[_, ..] => {
+                current.Message.push(s.clone())
+            }
         }
     }
-
-    //  U+0000
-    //
-    // reader
-    //     .lines()
-    //     .filter_map(|x|x.ok())
-    //     .for_each(|line| {
-    //         println!("{}", line);
-    //         match line.chars().collect::<Vec<char>>().as_slice() {
-    //             ['c', 'o', 'm', 'm', 'i', 't', ..] if current.commit_hash != "" => {
-    //                 if current != Default::default(){
-    //                     process(&current, &mut stats);
-    //                 }
-    //                 current = Default::default();
-    //                 current = chomp_commit(line, &current);
-    //             }
-    //             &[] => {}
-    //             &[_, ..] => {}
-    //         }
-    //     });
-    //
-    // process(&current, &mut stats);
+    process(&current, &mut stats);
 
     println!("Commit Count: {}", stats.commit_count);
 }
