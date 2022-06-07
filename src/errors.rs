76d1e734ec4adbd3ec664c885e1699cee6ffdafa
Andy Rea <email@andrewrea.co.uk>
2022-06-07 19:55:00 +0100

    Add tests for error

--- src/errors.rs
+++ src/errors.rs
@@ -24,13 +24,17 @@ pub enum ErrorType {
     ErrNotGitRepository
 }
 
+const ERR_EXECUTING_GIT_VALUE: &str = "Shtats Error: failed to execute git";
+const ERR_UNSAFE_GIT_REPOSITORY_VALUE: &str = "Shtats Error: Unsafe repository.  Please visit https://github.blog/2022-04-12-git-security-vulnerability-announced/";
+const ERR_NOT_GIT_REPOSITORY: &str = "Shtats Error: Not a git repository.";
+
 // Implement std::fmt::Display for AppError
 impl fmt::Display for ErrorType {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            ErrorType::ErrExecutingGit => write!(f, "Shtats Error: failed to execute git"),
-            ErrorType::ErrUnsafeGitRepository => write!(f, "Shtats Error: Unsafe repository.  Please visit https://github.blog/2022-04-12-git-security-vulnerability-announced/"),
-            ErrorType::ErrNotGitRepository => {write!(f, "Shtats Error: Not a git repository.")}
+            ErrorType::ErrExecutingGit => write!(f, "{}", ERR_EXECUTING_GIT_VALUE),
+            ErrorType::ErrUnsafeGitRepository => write!(f,"{}", ERR_UNSAFE_GIT_REPOSITORY_VALUE),
+            ErrorType::ErrNotGitRepository => {write!(f,"{}", ERR_NOT_GIT_REPOSITORY)}
         }
     }
 }
@@ -43,3 +47,27 @@ impl From<std::io::Error> for ShtatsError {
 
 
 impl Error for ShtatsError{}
+
+#[cfg(test)]
+mod tests{
+    use crate::errors::{ERR_EXECUTING_GIT_VALUE, ERR_NOT_GIT_REPOSITORY, ERR_UNSAFE_GIT_REPOSITORY_VALUE, ErrorType, ShtatsError};
+
+    #[test]
+    fn test_reg_error_display_for_err_executing_git(){
+        let error = ShtatsError::Regular(ErrorType::ErrExecutingGit);
+        assert_eq!(error.to_string(), ERR_EXECUTING_GIT_VALUE)
+    }
+
+
+    #[test]
+    fn test_reg_error_display_for_err_unsafe_git_repository(){
+        let error = ShtatsError::Regular(ErrorType::ErrUnsafeGitRepository);
+        assert_eq!(error.to_string(), ERR_UNSAFE_GIT_REPOSITORY_VALUE)
+    }
+
+    #[test]
+    fn test_reg_error_display_for_err_not_git_repository(){
+        let error = ShtatsError::Regular(ErrorType::ErrNotGitRepository);
+        assert_eq!(error.to_string(), ERR_NOT_GIT_REPOSITORY)
+    }
+}
